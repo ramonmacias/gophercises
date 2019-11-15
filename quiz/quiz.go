732662355b28c13fdff166deffccf2b729cf0b90
Ramon <ramonmaciasgarcia@gmail.com>
2019-11-15 07:41:58 +0100

    The first part for the quiz game without a timer

---
+++ quiz/quiz.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strconv"
+)
+
+const (
+	defaultFileName = "problems.csv"
+)
+
+func main() {
+	var totalQuestions, totalSuccessfulAnswers int
+
+	file, err := os.Open("problems.csv")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	r := csv.NewReader(file)
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		var answer int
+		log.Printf("%s ", record[0])
+		fmt.Scanln(&answer)
+		correctAnswer, err := strconv.Atoi(record[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+		if answer == correctAnswer {
+			totalSuccessfulAnswers++
+		}
+		totalQuestions++
+	}
+	log.Printf("Your result is total questions %d total correct answers %d", totalQuestions, totalSuccessfulAnswers)
+}
