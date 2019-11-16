6fc90b26b3cda3828839db7c8e0a6b4a24fd2f4a
Ramon <ramonmaciasgarcia@gmail.com>
2019-11-16 22:54:41 +0100

    Added a new go routine for scan, with this way we don't block on the select, also add new flags for select the file name and limit of time

--- quiz/quiz.go
+++ quiz/quiz.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"io"
 	"log"
@@ -10,20 +11,18 @@ import (
 	"time"
 )
 
-const (
-	defaultFileName         = "problems.csv"
-	defaultTimeLimitSeconds = 10
-)
-
 var (
 	cTimer        chan time.Time
 	cAnswer       chan int
 	cNextQuestion chan int
 	cError        chan error
+
+	fileName         = flag.String("file", "problems.csv", "This flag determine the file name to user")
+	timeLimitSeconds = flag.Int("limit", 10, "This is the limit of time in seconds to answer each question")
 )
 
 func quiz() {
-	file, err := os.Open("problems.csv")
+	file, err := os.Open(*fileName)
 	if err != nil {
 		cError <- err
 	}
@@ -43,24 +42,27 @@ func quiz() {
 			}
 			var answer int
 			log.Printf("%s ", record[0])
-			_, err = fmt.Scanln(&answer)
-			if err != nil {
-				cError <- err
-			}
-			correctAnswer, err := strconv.Atoi(record[1])
-			if err != nil {
-				cError <- err
-			}
-			if answer == correctAnswer {
-				cAnswer <- 1
-			} else {
-				cAnswer <- 0
-			}
+			go func() {
+				_, err = fmt.Scanln(&answer)
+				if err != nil {
+					cError <- err
+				}
+				correctAnswer, err := strconv.Atoi(record[1])
+				if err != nil {
+					cError <- err
+				}
+				if answer == correctAnswer {
+					cAnswer <- 1
+				} else {
+					cAnswer <- 0
+				}
+			}()
 		}
 	}
 }
 
 func main() {
+	flag.Parse()
 	var totalQuestions, totalSuccessfulAnswers int
 	var err error
 	var timer *time.Timer
@@ -69,28 +71,24 @@ func main() {
 	cError = make(chan error)
 
 	go quiz()
-	timer = time.NewTimer(time.Duration(defaultTimeLimitSeconds) * time.Second)
+	timer = time.NewTimer(time.Duration(*timeLimitSeconds) * time.Second)
 	for {
 		cNextQuestion <- 1
 		select {
 		case <-timer.C:
-			log.Println("Time expired")
-			timer.Reset(time.Duration(defaultTimeLimitSeconds) * time.Second)
-			log.Println("Timer reset")
+			timer.Reset(time.Duration(*timeLimitSeconds) * time.Second)
 		case result := <-cAnswer:
-			log.Println("Receive value")
 			if result == 1 {
 				totalSuccessfulAnswers++
 			}
 			timer.Stop()
-			timer.Reset(time.Duration(defaultTimeLimitSeconds) * time.Second)
+			timer.Reset(time.Duration(*timeLimitSeconds) * time.Second)
 		case err = <-cError:
 			if err != io.EOF {
 				log.Panic(err)
 			}
 			timer.Stop()
 		}
-		log.Println("Increment total questions counter")
 		totalQuestions++
 		if err != nil {
 			break
