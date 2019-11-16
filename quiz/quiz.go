90baeda09216bd27cdc6b4b5bd44021a14ecf97d
Ramon <ramonmaciasgarcia@gmail.com>
2019-11-16 05:32:51 +0100

    Added new channels and use select clausule in order to receive information from different channels, preparing the scenario for a timer

--- quiz/quiz.go
+++ quiz/quiz.go
@@ -7,44 +7,85 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"time"
 )
 
 const (
-	defaultFileName = "problems.csv"
+	defaultFileName         = "problems.csv"
+	defaultTimeLimitSeconds = 30
 )
 
-func main() {
-	var totalQuestions, totalSuccessfulAnswers int
+var (
+	cTimer        chan time.Time
+	cAnswer       chan int
+	cNextQuestion chan int
+	cError        chan error
+)
 
+func quiz() {
 	file, err := os.Open("problems.csv")
 	if err != nil {
-		panic(err)
+		cError <- err
 	}
 	defer file.Close()
 
 	r := csv.NewReader(file)
+
 	for {
+		<-cNextQuestion
 		record, err := r.Read()
 		if err == io.EOF {
-			break
+			cError <- err
 		}
 		if err != nil {
-			log.Fatal(err)
+			cError <- err
 		}
 		var answer int
 		log.Printf("%s ", record[0])
 		_, err = fmt.Scanln(&answer)
 		if err != nil {
-			log.Panic(err)
+			cError <- err
 		}
 		correctAnswer, err := strconv.Atoi(record[1])
 		if err != nil {
-			log.Fatal(err)
+			cError <- err
 		}
 		if answer == correctAnswer {
-			totalSuccessfulAnswers++
+			cAnswer <- 1
+		} else {
+			cAnswer <- 0
+		}
+	}
+}
+
+func main() {
+	var totalQuestions, totalSuccessfulAnswers int
+	var err error
+	cTimer = make(chan time.Time)
+	cAnswer = make(chan int)
+	cNextQuestion = make(chan int)
+	cError = make(chan error)
+
+	go quiz()
+	for {
+		cNextQuestion <- 1
+		select {
+		case <-cTimer:
+			log.Println("Time expired")
+		case result := <-cAnswer:
+			log.Println("Receive value")
+			if result == 1 {
+				totalSuccessfulAnswers++
+			}
+		case err = <-cError:
+			if err != io.EOF {
+				log.Panic(err)
+			}
 		}
 		totalQuestions++
+		if err != nil {
+			break
+		}
 	}
 	log.Printf("Your result is total questions %d total correct answers %d", totalQuestions, totalSuccessfulAnswers)
 }
