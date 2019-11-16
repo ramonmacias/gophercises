f6d4b507e1f7828df3b9d59a7359949e2a747d85
Ramon <ramonmaciasgarcia@gmail.com>
2019-11-16 06:11:51 +0100

    Trying to make a communication with the channel when the timer expires

--- quiz/quiz.go
+++ quiz/quiz.go
@@ -12,7 +12,7 @@ import (
 
 const (
 	defaultFileName         = "problems.csv"
-	defaultTimeLimitSeconds = 30
+	defaultTimeLimitSeconds = 10
 )
 
 var (
@@ -32,28 +32,30 @@ func quiz() {
 	r := csv.NewReader(file)
 
 	for {
-		<-cNextQuestion
-		record, err := r.Read()
-		if err == io.EOF {
-			cError <- err
-		}
-		if err != nil {
-			cError <- err
-		}
-		var answer int
-		log.Printf("%s ", record[0])
-		_, err = fmt.Scanln(&answer)
-		if err != nil {
-			cError <- err
-		}
-		correctAnswer, err := strconv.Atoi(record[1])
-		if err != nil {
-			cError <- err
-		}
-		if answer == correctAnswer {
-			cAnswer <- 1
-		} else {
-			cAnswer <- 0
+		select {
+		case <-cNextQuestion:
+			record, err := r.Read()
+			if err == io.EOF {
+				cError <- err
+			}
+			if err != nil {
+				cError <- err
+			}
+			var answer int
+			log.Printf("%s ", record[0])
+			_, err = fmt.Scanln(&answer)
+			if err != nil {
+				cError <- err
+			}
+			correctAnswer, err := strconv.Atoi(record[1])
+			if err != nil {
+				cError <- err
+			}
+			if answer == correctAnswer {
+				cAnswer <- 1
+			} else {
+				cAnswer <- 0
+			}
 		}
 	}
 }
@@ -61,27 +63,34 @@ func quiz() {
 func main() {
 	var totalQuestions, totalSuccessfulAnswers int
 	var err error
-	cTimer = make(chan time.Time)
+	var timer *time.Timer
 	cAnswer = make(chan int)
 	cNextQuestion = make(chan int)
 	cError = make(chan error)
 
 	go quiz()
+	timer = time.NewTimer(time.Duration(defaultTimeLimitSeconds) * time.Second)
 	for {
 		cNextQuestion <- 1
 		select {
-		case <-cTimer:
+		case <-timer.C:
 			log.Println("Time expired")
+			timer.Reset(time.Duration(defaultTimeLimitSeconds) * time.Second)
+			log.Println("Timer reset")
 		case result := <-cAnswer:
 			log.Println("Receive value")
 			if result == 1 {
 				totalSuccessfulAnswers++
 			}
+			timer.Stop()
+			timer.Reset(time.Duration(defaultTimeLimitSeconds) * time.Second)
 		case err = <-cError:
 			if err != io.EOF {
 				log.Panic(err)
 			}
+			timer.Stop()
 		}
+		log.Println("Increment total questions counter")
 		totalQuestions++
 		if err != nil {
 			break
