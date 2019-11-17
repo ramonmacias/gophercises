4de84b06f2f0c621ca58dbce6375b8db5fcdb16a
Ramon <ramonmaciasgarcia@gmail.com>
2019-11-17 01:29:06 +0100

    Use redirect http function when we find the path in our map

--- urlshort/handler.go
+++ urlshort/handler.go
@@ -11,8 +11,14 @@ import (
 // If the path is not provided in the map, then the fallback
 // http.Handler will be called instead.
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
-	//	TODO: Implement this...
-	return nil
+	return func(resp http.ResponseWriter, req *http.Request) {
+		val, exist := pathsToUrls[req.URL.Path]
+		if exist {
+			http.Redirect(resp, req, val, http.StatusMovedPermanently)
+		} else {
+			resp.WriteHeader(http.StatusOK)
+		}
+	}
 }
 
 // YAMLHandler will parse the provided YAML and then return
--- urlshort/main/main.go
+++ urlshort/main/main.go
@@ -19,18 +19,20 @@ func main() {
 
 	// Build the YAMLHandler using the mapHandler as the
 	// fallback
-	yaml := `
-    - path: /urlshort
-      url: https://github.com/gophercises/urlshort
-    - path: /urlshort-final
-      url: https://github.com/gophercises/urlshort/tree/solution
-    `
-	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
-	if err != nil {
-		panic(err)
-	}
+	// yaml := `
+	//   - path: /urlshort
+	//     url: https://github.com/gophercises/urlshort
+	//   - path: /urlshort-final
+	//     url: https://github.com/gophercises/urlshort/tree/solution
+	//   `
+	// yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println("Starting the server on :8080")
+	// http.ListenAndServe(":8080", yamlHandler)
 	fmt.Println("Starting the server on :8080")
-	http.ListenAndServe(":8080", yamlHandler)
+	http.ListenAndServe(":8080", mapHandler)
 }
 
 func defaultMux() *http.ServeMux {
